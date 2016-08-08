d27c93690b8dce42bf3403ee47157eee5b370f70
thatONEjustin <justinpaelmo@gmail.com>
2016-08-08 08:47:31 -0400

    gulpjs upgrade!

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+'use strict';
+
+//Main gulp pointer
+var gulp = require('gulp');
+
+//Plugins
+var cached = require('gulp-cached');
+var copy = require('gulp-copy');
+var watch = require('gulp-watch');
+var less = require('gulp-less');
+var cssmin = require('gulp-minify-css');
+var rename = require('gulp-rename');
+
+//LocalWebserver
+var webserver = require('gulp-webserver');
+//var webserver = require('gulp-connect');
+
+//File Paths
+var less_files = ['build/css/**.less', '!build/css/helpers.less'];
+var html_files = ['build/**/**.html'];
+var js_files   = ['build/js/**.js'];
+
+gulp.task('bootstrap', function () {
+    return gulp.src(['build/css/bootstrap.min.css'])
+            .pipe(cached('bootstrap'))
+            .pipe(copy('dist', { prefix: 1 }))
+            .on('error', outputError);
+});
+
+gulp.task('copy-html', function () {
+    return gulp.src(html_files)
+            .pipe(cached('copy'))
+            .pipe(copy('dist', { prefix: 1 }))
+            .on('error', outputError);
+});
+
+gulp.task('scripts', function () {
+    return gulp.src(js_files)
+            .pipe(cached('scripts'))
+            .pipe(copy('dist', { prefix: 1 }))
+            .on('error', outputError);
+});
+
+gulp.task('less', function () {
+    return gulp.src(less_files, { base: 'build' })
+            .pipe(cached('less'))
+            .pipe(less())
+            .on('error', outputError)
+            .pipe(gulp.dest('dist'));
+});
+
+gulp.task('cssmin', function () {
+    return gulp.src(['dist/css/**/**.css', '!dist/css/**.min.css'], { base: 'dist' })
+            .pipe(cached('cssmin'))
+            .pipe(cssmin())
+            .pipe(rename({ suffix: '.min'}))
+            .on('error', outputError)
+            .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function () {    
+    gulp.watch(less_files, ['less', 'cssmin']);
+    gulp.watch(html_files, ['copy-html']);
+    gulp.watch(js_files, ['scripts']);
+});
+
+gulp.task('webserver', function () {
+    gulp.src('dist')
+        .pipe(webserver({
+            livereload: true,
+            port: 8080,
+            open: true
+        }))
+});
+
+/*gulp.task('webserver', function () {
+    webserver.server({
+        root: 'public',
+        port: 3000,
+        host: '192.168.1.200',
+        fallback: 'index.html',
+        livereload: true
+    });
+});*/
+
+
+gulp.task('default', ['copy-html', 'scripts', 'bootstrap', 'less', 'cssmin', 'watch', 'webserver']);
+
+function outputError (error) {
+    console.log(error.toString());
+
+    this.emit('end');
+}
