e40eae810dc9f295fc5236931d4a037752db4e3b
thatONEjustin <justinpaelmo@gmail.com>
2016-08-08 15:12:48 -0400

    added image copy

--- gulpfile.js
+++ gulpfile.js
@@ -11,14 +11,26 @@ var less = require('gulp-less');
 var cssmin = require('gulp-minify-css');
 var rename = require('gulp-rename');
 
-//LocalWebserver
-var webserver = require('gulp-webserver'); 
-
 //File Paths
-var less_files = ['build/css/**.less', 'build/**.less', 'build/themes/**.less', '!build/themes/helpers.less'];
-var html_files = ['build/**/**.html', 'build/**/**.cfm', 'build/**.cfm'];
-var js_files   = ['build/js/**.js'];
 
+//site_files only used for the build task.
+var site_files = ['build/**/**.*', '!build/**/helpers.less'];
+
+//These are used for all the other tasks.
+var html_files = ['build/**/**.html', 'build/**/**.cfm', 'build/**.cfm'];
+var less_files = ['build/css/**.less', 'build/**.less', 'build/themes/**.less', '!build/themes/helpers.less'];
+var js_files   = ['build/js/**.js'];
+var image_files = ['build/img/**/**.{jpg,gif,png,svg}', 'build/img/**.{jpg,gif,png,svg}'];
+
+//Copy all site files over into the /dist/ directory.
+gulp.task('copy-build', function() {
+    return gulp.src(site_files)
+            .pipe(cached('build'))
+            .pipe(copy('dist', { prefix: 1 }))
+            .on('error', outputError);
+});
+
+//Copy over bootstrap.min.css file (enforcing standards)
 gulp.task('bootstrap', function () {
     return gulp.src(['build/css/bootstrap.min.css'])
             .pipe(cached('bootstrap'))
@@ -26,6 +38,15 @@ gulp.task('bootstrap', function () {
             .on('error', outputError);
 });
 
+//Copy over /img/ directory. 
+gulp.task('copy-img', function () {
+    return gulp.src(image_files)
+            .pipe(cached('copy-img'))
+            .pipe(copy('dist', { prefix: 1 }))
+            .on('error', outputError);
+});
+
+//Copy over any .html/.cfm type file over. 
 gulp.task('copy-html', function () {
     return gulp.src(html_files)
             .pipe(cached('copy'))
@@ -33,6 +54,7 @@ gulp.task('copy-html', function () {
             .on('error', outputError);
 });
 
+//Copy scripts/js over to /dist/
 gulp.task('scripts', function () {
     return gulp.src(js_files)
             .pipe(cached('scripts'))
@@ -40,6 +62,7 @@ gulp.task('scripts', function () {
             .on('error', outputError);
 });
 
+//Process .LESS files into .css 
 gulp.task('less', function () {
     return gulp.src(less_files, { base: 'build' })
             .pipe(cached('less'))
@@ -48,24 +71,29 @@ gulp.task('less', function () {
             .pipe(gulp.dest('dist'));
 });
 
+//Process .CSS files in /dist/ into .min.css
 gulp.task('cssmin', function () {
-    return gulp.src(['dist/**.css', 'dist/themes/**.css', '!dist/*.min.css', '!dist/themes/*.min.css'])
-            //.pipe(cached('cssmin'))
+    return gulp.src(['dist/**.css', 'dist/themes/**.css', '!dist/*.min.css', '!dist/themes/*.min.css'])            
             .pipe(cssmin())
             .pipe(rename({ suffix: '.min' }))
             .on('error', outputError)
             .pipe(gulp.dest(function(file) {
                 return file.base;
-            }));
+            }));   
 });
 
+//Current watch task.
 gulp.task('watch', function () {    
     gulp.watch(less_files, ['less', 'cssmin']);
     gulp.watch(html_files, ['copy-html']);
     gulp.watch(js_files, ['scripts']);
+    gulp.watch(image_files, ['copy-img']);
 });
 
-gulp.task('default', ['copy-html', 'scripts', 'bootstrap', 'less', 'cssmin', 'watch']);
+//Current build task.
+gulp.task('build', ['copy-build', 'scripts', 'less', 'cssmin']);
+
+gulp.task('default', ['watch']);
 
 function outputError (error) {
     console.log(error.toString());
